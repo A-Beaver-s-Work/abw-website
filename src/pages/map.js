793a5ece45a1aca8380c307d4476cbdf2ebc0d13
williamli0707 <williamhli0707@gmail.com>
2024-10-19 17:35:18 -0700

    larger map

--- src/pages/map.js
+++ src/pages/map.js
@@ -6,15 +6,13 @@ export default function Map() {
         <Layout>
         <div className="bg-light-gray h-full flex-grow">
             <div className={"h-14"}></div>
-            <div className="max-w-md mx-auto m-10">
+            <div className="max-w-[60vw] mx-auto m-10">
                 <div className="bg-green-800 shadow-lg rounded-lg overflow-hidden">
                     <div className="text-white font-light text-2xl mt-4 bg-green-800 p-4">
                         <p>Trees Planted</p>
                     </div>
-                    <div className="relative h-80 bg-[url('/img/map.png')] bg-cover bg-center">
-                        <div className="flex flex-col items-start justify-center h-full pl-4">
-
-                        </div>
+                    <div className="relative bg-cover bg-center">
+                        <img className={"w-full"} src="/img/map.png" />
                     </div>
                 </div>
             </div>
